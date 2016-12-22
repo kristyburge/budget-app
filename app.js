05708d4d20b856e7a6abc15f1bdda7ed9ad72684
Kristy Burge <kristyburge@gmail.com>
2016-12-22 12:35:37 -0700

    Create init function and read input data

--- app.js
+++ app.js
@@ -12,7 +12,32 @@ var budgetController = (function() {
 // UI CONTROLLER
 var UIController = (function() {
     
-    // some code
+    // create private variable/object to store DOM strings
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputButton: '.add__btn'
+    }
+    
+    // return an object that contains a method to get input values
+    return {
+      getInput: function() {
+          
+          return { // return an object with three properties instead of having 3 separate variables
+            type: document.querySelector(DOMstrings.inputType).value, // will be either income or expense
+            description: document.querySelector(DOMstrings.inputDescription).value,
+            value: document.querySelector(DOMstrings.inputValue).value
+          };
+          
+        }, 
+
+    // pass DOMstrings object to the global app controller
+    getDOMstrings: function() {
+        return DOMstrings;
+        }
+
+    };
     
 })();
 
@@ -20,10 +45,30 @@ var UIController = (function() {
 // GLOBAL APP CONTROLLER 
 var controller = (function(budgetCntrl, UICntrl) {
     
+    // private function that sets up the event listeners
+    var setUpEventListeners = function() {
+       
+        var DOM = UICntrl.getDOMstrings();
+        document.querySelector(DOM.inputButton).addEventListener('click', controlAddItem);
+    
+        document.addEventListener('keypress', function(event){
+        
+            // use .which to add support for older browsers
+            if (event.keyCode === 13 || event.which === 13){
+                controlAddItem(); 
+            }
+        
+        });
+    };
+    
+    
+      
+    // private function that gets called when we want to add a new item
     var controlAddItem = function(){
         
-        // 1. Get the field input data
-        console.log('Hello World!');
+        // 1. Get the field input data when enter key or button is clicked
+        var input = UICntrl.getInput();
+        console.log(input);
         
         // 2. Add the item to the budget controller
         
@@ -38,28 +83,21 @@ var controller = (function(budgetCntrl, UICntrl) {
         
     };
     
-    document.querySelector('.add__btn').addEventListener('click', controlAddItem);
     
+    // create a public initialization function
+    // return in an object to make public
     
-    document.addEventListener('keypress', function(event){
-        
-        // use .which to add support for older browsers
-        if (event.keyCode === 13 || event.which === 13){
-            controlAddItem(); 
+    return {
+        init: function() {
+            //console.log('Application has begun.');
+            setUpEventListeners();
         }
-        
-    });
+    }
+    
+    
     
 })(budgetController, UIController);
 
 
-
-
-
-
-
-
-
-
-
-
+// begin the app or nothing will ever run because the event listeners are in a private function
+controller.init();
